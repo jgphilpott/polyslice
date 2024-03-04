47db75963cbd345aee993b69e0bc4f6fa8e4e964
Jacob Philpott <jacob.philpott@elentra.com>
2024-03-04 18:02:16 +0300

    node serial example

---
+++ serial/node/usb.js
@@ -0,0 +1,21 @@
+const { SerialPort } = require("serialport")
+
+const port = new SerialPort({
+    path: "/dev/tty.usbserial-10",
+    baudRate: 115200
+})
+
+port.on("open", function() {
+
+    console.log("Opened!")
+
+    port.on("data", function(data) {
+        console.log("Received: " + data)
+    })
+
+    port.write("G28\n", function(error, results) {
+        console.log("Results: " + results)
+        console.log("Error: " + error)
+    })
+
+})
